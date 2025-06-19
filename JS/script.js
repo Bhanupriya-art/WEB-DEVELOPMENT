3d021c72d52eb7ee7c3815fe221e5c190b932b63
Bhanu Priya <120407422+Bhanupriya-art@users.noreply.github.com>
2025-06-19 20:46:18 +0530

    First Commit

--- JS/script.js
+++
@@ -1,5 +0,0 @@
-function changeColor() {
-  const colors = ["#f8b400", "#3ac569", "#2d87f0", "#f54291", "#ff5733"];
-  const randomColor = colors[Math.floor(Math.random() * colors.length)];
-  document.body.style.backgroundColor = randomColor;
-}
