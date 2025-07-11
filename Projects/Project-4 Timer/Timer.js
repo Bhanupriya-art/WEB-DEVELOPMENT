6bf146c1e9a0e3456246358b5f13a503f1f3f67a
Bhanu Priya <120407422+Bhanupriya-art@users.noreply.github.com>
2025-07-11 20:24:45 +0530

    Update Timer.js

--- Projects/Project-4 Timer/Timer.js
+++ Projects/Project-4 Timer/Timer.js
@@ -2,22 +2,20 @@ let timerDisplay = document.getElementById('timer');
 let startBtn = document.getElementById('startBtn');
 let stopBtn = document.getElementById('stopBtn');
 let resetBtn = document.getElementById('resetBtn');
-
 let timer = null;
 let seconds = 0;
-
-function formatTime(sec){         // This function takes total seconds and converts them into hours, minutes, and seconds. and math.floor -> rounds down the values.
+function formatTime(sec){     
     let hrs = Math.floor(sec /3600);
     let mins = Math.floor((sec % 3600) /60);
     let secs = sec % 60;
     return[
         hrs.toString().padStart(2, '0'),
         mins.toString().padStart(2,'0'),
-        secs.toString().padStart(2,'0')   //padStart(2, '0')-> ensures values are always 2 digits (e.g->5 becomes 05).
+        secs.toString().padStart(2,'0')
     ].join(':'); 
 }
 function startTimer() {
-    if(timer !== null) return; //avoid multiple intervals running at once
+    if(timer !== null) return; 
     timer = setInterval(() => {
         seconds++;
         timerDisplay.textContent = formatTime(seconds);
@@ -32,7 +30,6 @@ function resetTimer(){
     seconds =0;
     timerDisplay.textContent ="00:00:00";
 }
-
 startBtn.addEventListener('click',startTimer);
 stopBtn.addEventListener('click',stopTimer);
 resetBtn.addEventListener('click',resetTimer);
